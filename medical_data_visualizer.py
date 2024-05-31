5b81bb987255d86a7b08271a0ee851d404de530e
anil-banoth-2482 <anilb03032004@gmail.com>
2024-05-31 22:34:30 +0530

    completed catplot part

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -7,27 +7,33 @@ import numpy as np
 df = pd.read_csv("medical_examination.csv")
 
 # 2
-df['overweight'] =(df['weight']/((df['height']/100)**2)).apply(lambda x:1 if x>25 else 0)
+# df['overweight'] =(df['weight']/((df['height']/100)**2)).apply(lambda x:1 if x>25 else 0)
+df['overweight']=np.where((df['weight']/(df['height']/100)**2)>25,1,0)
 
 # 3
 df['cholesterol']=df['cholesterol'].apply(lambda x:0 if x==1 else 1)
 df['gluc']=df['gluc'].apply(lambda x:0 if x==1 else 1)
+#df.loc[df['cholesterol']==1,'cholesterol']=0
+#df.loc[df['cholesterol']>=2,'cholesterol']=1
+#df.loc[df['gluc']==1,'gluc']=0
+#df.loc[df['gluc']>=2,'gluc']=1
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df,id_vars='cardio',value_vars=['cholesterol','gluc','alco','active','smoke'],var_name='Feature',value_name='Value')
+    
 
     # 6
-    df_cat = None
+    df_cat =  df_cat.groupby(['cardio', 'Feature', 'Value']).size().reset_index(name='total')
     
 
     # 7
-
+    df_cat['Value'] = df_cat['Value'].astype(str)
+    g=sns.catplot(x='Feature',y='total',col='cardio',hue='value',kind="bar",data=df_cat)
 
 
     # 8
-    fig = None
+    fig = g.figure
 
 
     # 9
