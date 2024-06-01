c75a0c031438c107145df5f43b1656548eab709b
anil-banoth-2482 <anilb03032004@gmail.com>
2024-06-01 18:02:10 +0530

    finally completed project

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -8,11 +8,11 @@ df = pd.read_csv("medical_examination.csv")
 
 # 2
 # df['overweight'] =(df['weight']/((df['height']/100)**2)).apply(lambda x:1 if x>25 else 0)
-df['overweight']=np.where((df['weight']/(df['height']/100)**2)>25,1,0)
+df['overweight']=np.where((df['weight']/((df['height']/100)**2))>25,1,0)
 
 # 3
-df['cholesterol']=df['cholesterol'].apply(lambda x:0 if x==1 else 1)
-df['gluc']=df['gluc'].apply(lambda x:0 if x==1 else 1)
+df['cholesterol']=np.where(df['cholesterol']==1,0,1)
+df['gluc']=np.where(df['gluc']==1,0,1)
 #df.loc[df['cholesterol']==1,'cholesterol']=0
 #df.loc[df['cholesterol']>=2,'cholesterol']=1
 #df.loc[df['gluc']==1,'gluc']=0
@@ -20,16 +20,16 @@ df['gluc']=df['gluc'].apply(lambda x:0 if x==1 else 1)
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = pd.melt(df,id_vars='cardio',value_vars=['cholesterol','gluc','alco','active','smoke'],var_name='Feature',value_name='Value')
+    df_cat = pd.melt(df,id_vars='cardio',value_vars=['cholesterol','gluc','alco','active','smoke','overweight'])
     
 
     # 6
-    df_cat =  df_cat.groupby(['cardio', 'Feature', 'Value']).size().reset_index(name='total')
+    df_cat =  df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
     
 
     # 7
-    df_cat['Value'] = df_cat['Value'].astype(str)
-    g=sns.catplot(x='Feature',y='total',col='cardio',hue='value',kind="bar",data=df_cat)
+    df_cat['value'] = df_cat['value'].astype(str)
+    g=sns.catplot(x='variable',y='total',col='cardio',hue='value',kind="bar",data=df_cat)
 
 
     # 8
@@ -44,23 +44,23 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+        # Clean the data
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) & (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025)) & (df['weight'] <= df['weight'].quantile(0.975))]
+  
+    # Calculate the correlation matrix
+    corr_mat = df_heat.corr()
 
-    # 12
-    corr = None
+    # Generate a mask for the upper triangle
+    mask = np.zeros_like(corr_mat,dtype=bool)
+    mask[np.triu_indices_from(mask)] = True
+    
+    with sns.axes_style("white"):
+      fig, ax = plt.subplots( figsize=(10,8) )
+      sns.heatmap(corr_mat, mask=mask, annot=True, center=0, linewidths=.5, square=True,vmin=-0.15, vmax=0.3, fmt='0.1f')
 
-    # 13
-    mask = None
+      
+    # Do not modify the next two lines
+    
 
-
-
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
     fig.savefig('heatmap.png')
     return fig
